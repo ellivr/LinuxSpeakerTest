d68d243db461d95dd7aeb8d7c3c6fa585d31b5a7
ellivr <ellivr@users.noreply.github.com>
2015-10-21 00:06:47 +0700

    Create beep.sh
    pertamax

---
+++ beep.sh
@@ -0,0 +1,11 @@
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.1s ; kill -9 $pid
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.1s ; kill -9 $pid
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.2s ; kill -9 $pid
+sleep 0.5s;
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.1s ; kill -9 $pid
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.1s ; kill -9 $pid
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.2s ; kill -9 $pid
+sleep 0.5s;
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.1s ; kill -9 $pid
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.1s ; kill -9 $pid
+( speaker-test -t sine -f 1000 )& pid=$! ; sleep 0.2s ; kill -9 $pid
